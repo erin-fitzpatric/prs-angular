2ddd5d9565ce33714d4750e043a33a0b8b81d3da
erin-fitzpatric <efitzpatric@gmail.com>
2019-12-05 11:22:59 -0500

    added line-item list by ID

--- src/app/feature/request/request-lines/request-lines.component.ts
+++ src/app/feature/request/request-lines/request-lines.component.ts
@@ -3,6 +3,10 @@ import { LineItem } from 'src/app/model/line-item.class';
 import { LineItemService } from 'src/app/service/line-item.service';
 import { SystemService } from 'src/app/service/system.service';
 import { BaseComponent } from '../../base/base/base.component';
+import { RequestService } from 'src/app/service/request.service';
+import { JsonResponse } from 'src/app/model/json-response.class';
+import { ActivatedRoute } from '@angular/router';
+import { Request } from 'src/app/model/request.class';
 
 @Component({
   selector: 'app-request-lines',
@@ -10,14 +14,27 @@ import { BaseComponent } from '../../base/base/base.component';
   styleUrls: ['./request-lines.component.css']
 })
 export class RequestLinesComponent extends BaseComponent implements OnInit {
-  title: string = "Line-Items";
+  titleOne: string = "Request Line-Items";
+  titleTwo: string = "Line-Items";
+  request: Request = new Request;
   lineItems: LineItem[] = [];
+  id: number = 0;
+  jr: JsonResponse;
+
   constructor(private lineItemSvc: LineItemService,
-    protected sysSvc: SystemService) {
+    protected sysSvc: SystemService,
+    private requestSvc: RequestService,
+    private route: ActivatedRoute) {
     super(sysSvc);
   }
   ngOnInit() {
     super.ngOnInit();
+    // gets request by id
+    this.route.params.subscribe(parms => this.id = parms['id']);
+    this.requestSvc.get(this.id).subscribe(jr => {
+      this.request = jr.data as Request;
+    })
+
     console.log("Calling line item service list...");
     this.lineItemSvc.list().subscribe(jr => {
       this.lineItems = jr.data as LineItem[];
