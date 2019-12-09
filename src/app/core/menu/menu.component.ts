20232376cab4ea14705dd2ce3ff083330545095f
erin-fitzpatric <efitzpatric@gmail.com>
2019-12-09 02:17:11 -0500

    updated permissions for isReviewer and isAdmin

--- src/app/core/menu/menu.component.ts
+++ src/app/core/menu/menu.component.ts
@@ -1,26 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuItem } from 'src/app/model/menu-item.class';
+import { BaseComponent } from 'src/app/feature/base/base/base.component';
+import { SystemService } from 'src/app/service/system.service';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent extends BaseComponent implements OnInit {
   menuItems: MenuItem[] = [];
-  constructor() { }
+  constructor(protected sysSvc: SystemService) {
+    super(sysSvc);
+  }
 
   ngOnInit() {
+    super.ngOnInit();
+    this.sysSvc.checkLogin();
     this.menuItems = [
-      new MenuItem("Home","/home", "Home"),
-      new MenuItem("User","/users/list", "Users List"),
-      new MenuItem("Vendor","/vendors/list", "Vendors List"),
-      new MenuItem("Product","/products/list", "Products List"),
-      new MenuItem("Request","/requests/list", "Requests List"),
-      new MenuItem("Review","/requests/review", "Reviews List"),
-      new MenuItem("Login","/users/login", "Login"),
+      new MenuItem("Home", "/home", "Home"),
+      new MenuItem("User", "/users/list", "Users List"),
+      new MenuItem("Vendor", "/vendors/list", "Vendors List"),
+      new MenuItem("Product", "/products/list", "Products List"),
+      new MenuItem("Request", "/requests/list", "Requests List"),
       // new MenuItem("About","/about", "About"),
     ];
+
+    if (this.sysSvc.isReviewer()) {
+      this.menuItems.push(new MenuItem("Review", "/requests/review", "Reviews List"));
+    }
+    this.menuItems.push(new MenuItem("Login", "/users/login", "Login"));
   }
 
 }
--- src/app/feature/product/product-edit/product-edit.component.ts
+++ src/app/feature/product/product-edit/product-edit.component.ts
@@ -5,6 +5,8 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { BaseComponent } from '../../base/base/base.component';
 import { SystemService } from 'src/app/service/system.service';
+import { Vendor } from 'src/app/model/vendor.class';
+import { VendorService } from 'src/app/service/vendor.service';
 
 @Component({
   selector: 'app-product-edit',
@@ -15,8 +17,10 @@ export class ProductEditComponent extends BaseComponent implements OnInit {
   title: string = "Product Edit";
   product: Product = new Product();
   id: number = 0;
+  vendors: Vendor[] = [];
   constructor(private productSvc: ProductService,
               private router: Router,
+              private vendorSvc: VendorService,
               private route: ActivatedRoute,
               private loc: Location,
               protected sysSvc: SystemService) {
@@ -25,10 +29,14 @@ export class ProductEditComponent extends BaseComponent implements OnInit {
   ngOnInit() {
     super.ngOnInit();
     this.sysSvc.checkLogin();
+    // populate list of vendors
+    this.vendorSvc.list().subscribe(jr => {
+      this.vendors = jr.data as Vendor[];
+    });
     this.route.params.subscribe(parms => this.id = parms['id']);
     this.productSvc.get(this.id).subscribe(jr => {
       this.product = jr.data as Product;
-    })
+    });
   }
   save(): void {
     this.productSvc.save(this.product).subscribe(jr => {
@@ -40,5 +48,8 @@ export class ProductEditComponent extends BaseComponent implements OnInit {
   }
   backClicked(){
     this.loc.back();
+  }
+  compVendor(a: Vendor, b: Vendor): boolean {
+    return a && b && a.id === b.id;
   }
 }
